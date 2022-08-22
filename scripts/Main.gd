54b8c207a29d7183a72c91643161b633e5e422b7
Jelared <a.jade.mobile@gmail.com>
2022-08-23 00:29:10 +0200

    Update 23-08-2022
    fct choix du mot
    fct séparation des lettres
    nettoyage du code

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -1,38 +1,43 @@
 extends Control
 
+#regroupement de bouttons
 export(ButtonGroup) var GroupeClavier
+
+#variables
+var LettreClavier
+var Mot
+var Mot_separeL = []
+
+#listes des mots
+var list = [  "apple","turnip","banana","clock","cup","flower","fork","glasses","oranage","pencil","spoon"]
 
 
 func _ready():
-	
-	
-	
-	
-	
-	
+	bouttons()
+	choix_mot()
+	separation_mot()
+
+
+
+
+#l'appuie sur un bouton envoie un signal qui met en mémoire la lettre afficher sur le bouton et bloque le bouton qui ne peut plus etre utilisé
+func bouttons():
 	for button in GroupeClavier.get_buttons():
 		button.connect("pressed",self,"lettre")
-	
-	
+
 func lettre():
-	var LettreClavier = GroupeClavier.get_pressed_button().text
+	LettreClavier = GroupeClavier.get_pressed_button().text
 	GroupeClavier.get_pressed_button().disabled = true
+	print(LettreClavier)
 	
-	var TexteMy = "pendu"
+#Choix d'un mot dans une liste  : aléatoire randi(), entre %, 0 et le nombre total de mot dans la liste size()
+func choix_mot():
+	Mot = list[randi()%list.size()]
+	print(Mot)
 	
-	if LettreClavier in TexteMy:
-		print ("vrai")
 
-
-
-
-
-##Chooses a random word from the list array 
-#func choose_random_word():
-#	selected_word = list[randi()%list.size()]
-#	print(selected_word)
-#
-##Creates a new array with the choosen word seperating each letter into a new part of the array
-#func breakdown_random_word():
-#	for letters in selected_word:
-#		selected_word_breakdown.append(letters)
+#Séparation des lettres du mot choisi : boucle for, dans la liste mot, variable lettreM, dans la liste Mot_separeL, append ajoute les lettreM 
+func separation_mot():
+	for lettreM in Mot:
+		Mot_separeL.append(lettreM)
+		print(Mot_separeL)
