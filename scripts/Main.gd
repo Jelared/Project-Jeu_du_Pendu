b1d420c34a125b1d0f9299c6b08a3267b22322bf
Jelared <a.jade.mobile@gmail.com>
2022-08-29 18:29:33 +0200

    update 29-08-2022
    Fonction test ok
    lettres double à coder

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -1,16 +1,29 @@
 extends Control
 
-#regroupement de bouttons
-export(ButtonGroup) var GroupeClavier
+# Regroupement de bouttons
+export(ButtonGroup) var groupe_clavier
 
-#variables
-var LettreClavier
-var Mot
-var Mot_separeL = []
-var affichage = []
+#Variables
+var lettre_clavier #lettre choisie
+var mot #mot à trouver
+var mot_separeL = [] #array avec les lettres séparées
+var affichage = [] #Array avec les lettres trouvées
+var affichage_ = "" #String avec les lettres trouvées
 
-#listes des mots
-var list = [  "apple","turnip","banana","clock","cup","flower","fork","glasses","oranage","pencil","spoon"]
+#Listes des mots
+var list = [
+	"apple",
+	"turnip",
+	"banana",
+	"clock",
+	"cup",
+	"flower",
+	"fork",
+	"glasses",
+	"oranage",
+	"pencil",
+	"spoon"
+	]
 
 
 func _ready():
@@ -19,53 +32,57 @@ func _ready():
 	initialisation()
 	bouttons()
 
-#l'appuie sur un bouton envoie un signal 
+#L'appuie sur un bouton envoie un signal 
 func bouttons():
-	for button in GroupeClavier.get_buttons():
+	for button in groupe_clavier.get_buttons():
 		button.connect("pressed",self,"lettre")
 
-#le signal : met en mémoire la lettre (en minuscule) affiché sur le bouton et bloque le bouton qui ne peut plus etre utilisé
+#Le signal : met en mémoire la lettre (en minuscule) affiché sur le bouton et bloque le bouton qui ne peut plus etre utilisé
 func lettre():
-	LettreClavier = GroupeClavier.get_pressed_button().text.to_lower()
-	GroupeClavier.get_pressed_button().disabled = true
-	print(LettreClavier)
+	lettre_clavier = groupe_clavier.get_pressed_button().text.to_lower()
+	groupe_clavier.get_pressed_button().disabled = true
+	print(lettre_clavier)
 	test()
 	
 	
 #Choix d'un mot dans une liste  : aléatoire randi(), entre %, 0 et le nombre total de mot dans la liste size()
 func choix_mot():
 	randomize()
-	Mot = list[randi()%list.size()]
-	print(Mot)
+	mot = list[randi()%list.size()]
+	print(mot)
 	
 
 #Séparation des lettres du mot choisi : boucle for, dans la liste mot, variable lettreM, dans la liste Mot_separeL, append ajoute les lettreM 
 func separation_mot():
-	for lettreM in Mot:
-		Mot_separeL.append(lettreM)
-		print(Mot_separeL)
+	for lettreM in mot:
+		mot_separeL.append(lettreM)
+		print(mot_separeL)
 		
 
-#affichae des underscore au départ
+#affichage des underscore au départ
 func initialisation():
-	for i in Mot_separeL:
+	for i in mot_separeL:
 		affichage.append("_ ")
 	for i in affichage:
-		$Affichage.text += String(i)
+		affichage_ += String(i)
+		$Affichage.text = affichage_
 		print (affichage)
-		
 
 
 #test ==> A revoir
 func test():
-	if Mot_separeL.find(LettreClavier) == -1:
+	var position_L = mot_separeL.find(lettre_clavier)
+	if position_L == -1:
 		print("no")
-		print (Mot_separeL.find(LettreClavier))
+		print (position_L)
 	else :
-		#revoir la phase ci dessous car insert décale : il faut juste mettre la lettre à la bonne place => revir les array add position
-		affichage.insert(Mot_separeL.find(LettreClavier), LettreClavier)
-		$Affichage.text = str(affichage)
+		#attention au lettre double à revoir
+		affichage_ = ""
+		affichage[position_L] = lettre_clavier
+		for i in affichage:
+			affichage_ += String(i)
+			$Affichage.text = affichage_
 		print("ok")
-		print (Mot_separeL.find(LettreClavier))
+		print (mot_separeL.find(lettre_clavier))
 		print (affichage)
 		
